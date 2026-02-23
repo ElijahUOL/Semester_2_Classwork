f09c7f3af8aea278e52cb0d3ef1789205b5400f8
Elijah <dkwn0623@leeds.ac.uk>
2026-02-23 10:39:07 +0000

    completed worksheet 2.4

--- week_04/worksheet/portfolio/makeString.c
+++ week_04/worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Elijah Luehrmann
+Student ID: 201942447
 */
 
 #include <stdio.h>
@@ -10,9 +10,14 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-
+    char buffer[49]; // max length of all command line arguments is 4*10 and would have 9 '-' added so max string length is 49
     // process the command-line data using appropriate string functions
-
+    for(int i = 1; i < argc; i++){
+        strcat(buffer, argv[i]);
+        if (i != argc-1){
+            strcat(buffer, "-");
+        }
+    }
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
